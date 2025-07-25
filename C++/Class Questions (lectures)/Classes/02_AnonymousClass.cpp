28849ae4e562ee49ce538db7e078212adc9b870a
abhishekkksharma <abhisheksharma7340733@gmail.com>
2025-07-25 10:06:58 +0530

    add pointers to class  ques

--- C++/Class Questions (lectures)/Classes/02_AnonymousClass.cpp
+++ C++/Class Questions (lectures)/Classes/02_AnonymousClass.cpp
@@ -17,3 +17,17 @@ int main() {
     obj1.getData();
     return 0;
 }
+
+///when two or more than two objects communicate with each oyher we call them Association 
+// in Association there is not lifetime dependency 
+// there are two types of Association 
+//1. composition-
+
+//When two or more than two objects r trying to communicate with each other, it is known as ASSOCIATION...
+// **In association, there is no life time dependency.
+
+//TWO TYPES OF ASSOCIATION:
+
+//If container object(composite) doesn't exist then contained object(composed) doesn't exist...COMPOSITION(Car-Engine)
+//[In a whole-part relationship, if the whole doesn't exist, the part will cease to exist]
+//If container object doesn't exist , contained object can exist...AGGREGATION(College-Student)
